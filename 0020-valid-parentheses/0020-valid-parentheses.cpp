6ee6ed5d59f0d73d3c3f33888ba353174a1b991a
Rohit kumar <73387772+RohitKumar-tech@users.noreply.github.com>
2026-02-22 19:39:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else 
+            {
+                if(st.empty()) {
+                return false;
+            }
+                char top = st.top();
+                st.pop();
+                if(ch == ')' && top!='(') return false;
+                if(ch == ']' && top!='[') return false;
+                if(ch == '}' && top!='{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
