4cdbd0c8e82a574b3070750981cf5e066696bfeb
Rohit kumar <73387772+RohitKumar-tech@users.noreply.github.com>
2026-02-27 13:29:15 +0530

    Time: 34 ms (92.7%), Space: 54.1 MB (30.13%) - LeetHub

---
+++ 2775-generate-fibonacci-sequence/2775-generate-fibonacci-sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let current = 0;
+    let next = 1;
+
+    while(true){
+        yield current;
+
+        [current,next] = [next,current+next];
+
+    }
+
+
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
