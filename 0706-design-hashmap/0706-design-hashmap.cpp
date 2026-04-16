fd00e6eda68740dd4a7ea2781b544d3fd3c34abf
Rohit kumar <73387772+RohitKumar-tech@users.noreply.github.com>
2026-04-16 19:33:21 +0530

    Time: 61 ms (32.31%), Space: 220.3 MB (35.03%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,20 @@
+class MyHashMap {
+public:
+    vector<int> map;
+
+    MyHashMap() {
+        map = vector<int>(1000001, -1);
+    }
+    
+    void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    int get(int key) {
+        return map[key];
+    }
+    
+    void remove(int key) {
+        map[key] = -1;
+    }
+};
